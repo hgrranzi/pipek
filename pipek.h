32982c80d2affd653b0ed518921d69a133d5529f
hgrranzi <vkh.mariia@gmail.com>
2021-07-19 15:05:24 +0300

    Created the header

---
+++ pipek.h
@@ -0,0 +1,16 @@
+/* *********************************************** */
+/*                                                 */
+/*       by: hgrranzi <vkh.mariia@gmail.com>       */
+/*                                                 */
+/* *********************************************** */
+
+#ifndef PIPEK_H
+# define PIPEK_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <string.h>
+
+#endif
