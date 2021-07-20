07f801ca344d7a175549091defa75b6c47804430
hgrranzi <vkh.mariia@gmail.com>
2021-07-20 10:13:44 +0300

    Created an error writing function

--- main.c
+++ main.c
@@ -6,8 +6,30 @@
 
 #include "pipek.h"
 
+void	error_and_exit(char *error_message)
+{
+	char	*system_message;
+
+	system_message = strerror(errno);
+	if (error_message)
+		write(2, error_message, strlen(error_message));
+	else
+		write(2, system_message, strlen(system_message));
+	exit(0);
+}
+
+void	check_args(int argc, char **argv, t_cmd *cmd, char **files)
+{
+	if (argc < 5)
+		error_and_exit(ERR_ARGS);
+}
+
 int	main(int argc, char **argv)
 {
+	char	*files[2];
+	t_cmd	cmd;
 
+
+	check_args(argc, argv, &cmd, files);
 	return (0);
 }
--- pipek.h
+++ pipek.h
@@ -8,9 +8,22 @@
 # define PIPEK_H
 
 # include <unistd.h>
+# include <stdlib.h>
 # include <fcntl.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <string.h>
+# include <stdio.h>
+# include <errno.h>
+
+# define ERR_ARGS "Too few arguments\n"
+
+typedef struct	s_cmd
+{
+	char	*name;
+	char	**args;
+	int		fd[2];
+
+}				t_cmd;
 
 #endif
